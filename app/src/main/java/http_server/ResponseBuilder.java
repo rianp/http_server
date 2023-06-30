f31b406ae782d670986b43860a600dd098fbd6af
rian <rian@Ryans-MacBook-Pro.local>
2023-06-30 13:11:41 -0700

    fix status message to align with status code

--- app/src/main/java/http_server/ResponseBuilder.java
+++ app/src/main/java/http_server/ResponseBuilder.java
@@ -14,11 +14,11 @@ public class ResponseBuilder {
     String method = request.getMethod();
 
     if (!routeValidator.hasPath(path)) {
-      return handler.buildUnexpectedResponse("404");
+      return handler.buildUnexpectedResponse("404 Not Found");
     }
 
     if (!routeValidator.hasMethod(path, method)) {
-      return handler.buildMethodOptionsResponse("allow", "GET, HEAD, OPTIONS", "405");
+      return handler.buildMethodOptionsResponse("allow", "GET, HEAD, OPTIONS", "405 Method Not Allowed");
     }
 
     if (path != null && method != null) {
@@ -49,52 +49,52 @@ public class ResponseBuilder {
       }
     }
 
-    return handler.buildUnexpectedResponse("405");
+    return handler.buildUnexpectedResponse("405 Method Not Allowed");
   }
 
   private Response handleSimpleGetWithBodyResponse(String method) {
     if (method.equals("HEAD")) {
-      return handler.buildHeadResponse("200");
+      return handler.buildHeadResponse("200 OK");
     } else {
-      return handler.buildResponse("200", "Hello world");
+      return handler.buildResponse("200 OK", "Hello world");
     }
   }
 
   private Response handleSimpleGetResponse(String method) {
     if (method.equals("HEAD")) {
-      return handler.buildHeadResponse("200");
+      return handler.buildHeadResponse("200 OK");
     } else {
-      return handler.buildResponse("200", "");
+      return handler.buildResponse("200 OK", "");
     }
   }
 
   private Response handleEchoBodyResponse(String method, String body) {
     if (method.equals("POST") && body != null) {
-      return handler.buildResponse("200", body);
+      return handler.buildResponse("200 OK", body);
     }
-    return handler.buildUnexpectedResponse("405");
+    return handler.buildUnexpectedResponse("405 Method Not Allowed");
   }
 
   private Response handleHeadRequestResponse(String method) {
     if (method.equals("HEAD")) {
-      return handler.buildHeadResponse("200");
+      return handler.buildHeadResponse("200 OK");
     } else {
-      return handler.buildNonHeadResponse("200");
+      return handler.buildNonHeadResponse("200 OK");
     }
   }
 
   private Response handleMethodOptionsResponse(String method, String allowedMethods) {
     if (method.equals("OPTIONS")) {
-      return handler.buildMethodOptionsResponse("allow", allowedMethods, "200");
+      return handler.buildMethodOptionsResponse("allow", allowedMethods, "200 OK");
     }
-    return handler.buildUnexpectedResponse("405");
+    return handler.buildUnexpectedResponse("405 Method Not Allowed");
   }
 
   private Response handleRedirectResponse(String method) {
     if (method.equals("GET")) {
-      return handler.buildRedirectResponse("location", "http://127.0.0.1:8080/simple_get", "301");
+      return handler.buildRedirectResponse("location", "http://127.0.0.1:8080/simple_get", "301 Moved Permanently");
     }
-    return handler.buildUnexpectedResponse("405");
+    return handler.buildUnexpectedResponse("405 Method Not Allowed");
   }
 }
 
